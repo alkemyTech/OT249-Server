07579f8d8fc774def6fc39a83fd515f745cb864c
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-07-14 17:40:35 -0300

    OT249-18 Id Category

--- src/main/java/com/alkemy/ong/model/Category.java
+++ src/main/java/com/alkemy/ong/model/Category.java
@@ -2,13 +2,16 @@ package com.alkemy.ong.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "categories")
 @Entity
 @Data
 public class Category {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
     private String name;
     private String description;
     private String image;
