c4284bf036d7590f7c63a8c87c3de48f9c5fa3a0
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-07-14 17:38:03 -0300

    OT249-18 Excluding dataConfig

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -2,8 +2,10 @@ package com.alkemy.ong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// I add exclude annotation for run the app without DB config
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class OngApplication {
 
 	public static void main(String[] args) {
