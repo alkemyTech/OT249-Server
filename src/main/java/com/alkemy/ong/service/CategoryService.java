c78a047bab2a9e3f10996a4780bbc8a9386f5ae1
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-07-14 17:37:41 -0300

    OT249-18 Category service and its implementation

---
+++ src/main/java/com/alkemy/ong/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.service;
+
+import com.alkemy.ong.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    Category getCategory(Long id);
+    List<Category> getAllCategories();
+    void deleteCategory(Long id);
+    Category updateCategory(Category category, Long id);
+}
