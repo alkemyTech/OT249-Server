5f96de34ff2551337ad95c5551a29003b52f60ee
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-07-15 11:37:09 -0300

    Adding id annotation

--- src/main/java/com/alkemy/ong/model/Category.java
+++ src/main/java/com/alkemy/ong/model/Category.java
@@ -14,6 +14,7 @@ import java.sql.Timestamp;
 @Where(clause = "deleted=false")
 public class Category {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(nullable = false)
