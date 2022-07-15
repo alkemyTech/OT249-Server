ec51e56d67e34ed0d8514541448e5360bb5b7b18
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-07-15 11:37:53 -0300

    Adding db config

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// I add exclude annotation for run the app without DB config
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class OngApplication {
 
 	public static void main(String[] args) {
