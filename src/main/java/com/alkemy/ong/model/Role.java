77c5a109d45b84fa863d73cfe28b506474466fa0
Guido Nicolini <guidonicolini123@hotmail.com>
2022-07-17 13:34:37 -0300

    added uuid and users attribute

--- src/main/java/com/alkemy/ong/model/Role.java
+++ src/main/java/com/alkemy/ong/model/Role.java
@@ -1,13 +1,17 @@
 package com.alkemy.ong.model;
 
 import java.sql.Timestamp;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.Data;
 
@@ -17,11 +21,15 @@ import lombok.Data;
 public class Role {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
 
 	@Column(nullable = false)
 	private String name;
+
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	private private Set<User> users;
 
 	private String description;
 
--- src/main/java/com/alkemy/ong/repository/RoleRepository.java
+++ src/main/java/com/alkemy/ong/repository/RoleRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.alkemy.ong.model.Role;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, String> {
 
 }
