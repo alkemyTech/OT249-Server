8d0b003eed741c7764fe9efc8f84332ae27e916b
Emanuel991 <emanuelsosa@gmail.com>
2022-07-19 16:07:06 -0300

    Change String type by UUID type

--- src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
@@ -1,10 +1,12 @@
 package com.alkemy.ong.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.alkemy.ong.model.Testimonial;
 
 @Repository
-public interface TestimonialRepository extends JpaRepository<Testimonial, String>{
+public interface TestimonialRepository extends JpaRepository<Testimonial, UUID>{
 
 }
