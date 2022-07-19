a9f41859b5131ef25ab7b4f9663c11146b63754a
Emanuel991 <emanuelsosa@gmail.com>
2022-07-19 16:15:15 -0300

    Add configuration for role based access

--- src/main/java/com/alkemy/ong/security/ApiSecurityConfig.java
+++ src/main/java/com/alkemy/ong/security/ApiSecurityConfig.java
@@ -3,15 +3,25 @@ package com.alkemy.ong.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
-public class ApiSecurityConfig {
+public class ApiSecurityConfig extends WebSecurityConfigurerAdapter{
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+    
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/auth/login").permitAll()
+		.anyRequest().hasRole("USER");
     }
 
 }
