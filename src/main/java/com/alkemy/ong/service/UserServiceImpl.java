3b09957d6e3ca3a6e15aeb03247d49a173d06b6f
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-19 22:00:26 -0300

    OT249-35-Implemented method getAllUsers with signature (page, order) that returns a Page<User> from UserRepository.

--- src/main/java/com/alkemy/ong/service/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.alkemy.ong.service;
 
 
+import com.alkemy.ong.Utils.PageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +20,10 @@ public class UserServiceImpl implements UserService {
 	@Transactional
 	public User guardarUsuario(User user) {
 		return userRepo.save(user);
-	}	
+	}
+
+	@Override
+	public Page<User> getAllUsers(int page, String order) {
+		return userRepo.findAll( PageUtils.getPageable(page, order) );
+	}
 }
