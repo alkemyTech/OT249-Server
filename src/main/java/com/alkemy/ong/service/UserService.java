190006a510cffbdbea0eb46d13012166fbf94b81
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-19 21:59:49 -0300

    OT249-35-Added method getAllUsers with signature (page, order) that returns a Page<User>

--- src/main/java/com/alkemy/ong/service/UserService.java
+++ src/main/java/com/alkemy/ong/service/UserService.java
@@ -1,9 +1,11 @@
 package com.alkemy.ong.service;
 
 import com.alkemy.ong.model.User;
+import org.springframework.data.domain.Page;
 
 public interface UserService {
 
-	public User guardarUsuario(User user);
-		
+	User guardarUsuario(User user);
+
+    Page<User> getAllUsers(int page, String order);
 }
