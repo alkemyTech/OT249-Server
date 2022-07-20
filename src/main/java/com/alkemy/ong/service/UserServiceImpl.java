7961f097d36779a3831ab1d52b9e7522450d7325
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-19 22:13:18 -0300

    OT249-35-Re-Implemented a getAllUsers to return Page<UserDTO>.

--- src/main/java/com/alkemy/ong/service/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.service;
 
 
 import com.alkemy.ong.Utils.PageUtils;
+import com.alkemy.ong.dtos.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,18 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public Page<User> getAllUsers(int page, String order) {
-		return userRepo.findAll( PageUtils.getPageable(page, order) );
+	public Page<UserDTO> getAllUsers(int page, String order) {
+
+		Page<User> users = userRepo.findAll( PageUtils.getPageable( page, order ) );
+		Page<UserDTO> userDTOPage = users.map( user -> {
+			UserDTO userDTO = new UserDTO();
+			userDTO.setEmail( user.getEmail() );
+			userDTO.setFirstName( user.getFirstName() );
+			userDTO.setPhoto( user.getPhoto() );
+			userDTO.setLastName( user.getLastName() );
+			userDTO.setTimestamp( user.getTimestamp() );
+			return userDTO;
+		} );
+		return userDTOPage;
 	}
 }
