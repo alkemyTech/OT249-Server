8cd75cd9ca133037a79395f7ae3e7fb3e68e49a7
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-21 20:51:33 -0300

    OT249-25-Migration to RoleDto using ModelMapper

---
+++ src/main/java/com/alkemy/ong/dto/RoleDto.java
@@ -0,0 +1,31 @@
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Column;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDto implements GrantedAuthority {
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private Timestamp timestamp;
+
+    @Override
+    public String getAuthority() {
+
+        return this.getName();
+    }
+
+}
--- src/main/java/com/alkemy/ong/model/Role.java
+++ src/main/java/com/alkemy/ong/model/Role.java
@@ -19,7 +19,7 @@ import org.springframework.security.core.GrantedAuthority;
 @Table(name = "roles")
 @Entity
 @Data
-public class Role implements GrantedAuthority {
+public class Role {
 
 	@Id
 	@GeneratedValue(generator = "uuid")
@@ -29,13 +29,10 @@ public class Role implements GrantedAuthority {
 	@Column(nullable = false)
 	private String name;
 
+	@OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+	private Set<User> users;
+
 	private String description;
 
 	private Timestamp timestamp;
-
-	@Override
-	public String getAuthority() {
-
-		return this.getName();
-	}
 }
