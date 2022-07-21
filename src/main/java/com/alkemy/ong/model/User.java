0063c2380511a1fdb91ee62d6b7a97a51e401fa0
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-21 13:08:04 -0300

    OT249-25-Added lombok for constructor on user class

--- src/main/java/com/alkemy/ong/model/User.java
+++ src/main/java/com/alkemy/ong/model/User.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -21,94 +23,79 @@ import java.util.UUID;
 @Where(clause = "deleted=false")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable, UserDetails {
 
-	private static final long serialVersionUID = 1L;	
-	
-	@Id
-	private UUID id;
-	
-	@Column(name = "first_name", nullable = false)
-	@NotBlank(message = "Este campo no puede estar vacio")
-	private String firstName;
-	
-	@Column(name = "last_name", nullable = false)
-	@NotBlank(message = "Este campo no puede estar vacio")
-	private String lastName;
-	
-	@Column(unique = true, nullable = false)
-	@Email(message = "Email no valido")
-	private String email;
-	
-	@Column(nullable = false)
-	@NotBlank(message = "Este campo no puede estar vacio")
-	private String password;
-	
-	@NotBlank(message = "Este campo no puede estar vacio")
-	@Column(nullable = false)
-	private String photo;
-	
-	@ManyToOne
-	@JoinColumn(name = "rol_id", referencedColumnName = "id" )
-	private Role role;
-	
-	private Timestamp timestamp; 
-	
-	private boolean deleted = Boolean.FALSE;
-	
-	public User() {
-	}
+    private static final long serialVersionUID = 1L;
 
-	public User(UUID id ,String firstName, String lastName, String email, String password, String photo, Role rol, Timestamp timestamp, boolean deleted) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-		this.photo = photo;
-		this.role = rol;
-		this.timestamp = timestamp;
-		this.deleted = deleted;
-	}
+    @Id
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "Este campo no puede estar vacio")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "Este campo no puede estar vacio")
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    @Email(message = "Email no valido")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Este campo no puede estar vacio")
+    private String password;
+
+    @NotBlank(message = "Este campo no puede estar vacio")
+    @Column(nullable = false)
+    private String photo;
+
+    @ManyToOne
+    @JoinColumn(name = "rol_id", referencedColumnName = "id")
+    private Role role;
+
+    private Timestamp timestamp;
+
+    private boolean deleted = Boolean.FALSE;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return List.of( this.getRole() );
+    }
 
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
+    @Override
+    public String getUsername() {
 
-		return List.of(this.getRole());
-	}
+        return this.getEmail();
+    }
 
+    @Override
+    public boolean isAccountNonExpired() {
 
+        return !this.deleted;
+    }
 
-	@Override
-	public String getUsername() {
+    @Override
+    public boolean isAccountNonLocked() {
 
-		return this.getEmail();
-	}
+        return !this.deleted;
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
+    @Override
+    public boolean isCredentialsNonExpired() {
 
-		return !this.deleted;
-	}
+        return !this.deleted;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
+    @Override
+    public boolean isEnabled() {
 
-		return !this.deleted;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-
-		return !this.deleted;
-	}
-
-	@Override
-	public boolean isEnabled() {
-
-		return !this.deleted;
-	}
+        return !this.deleted;
+    }
 
 
 }
