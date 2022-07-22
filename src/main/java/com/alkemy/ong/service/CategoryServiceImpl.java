c28464a29a452c82ea68c732a64bdb4025d3e62e
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-22 19:17:07 -0300

    OT249-40 Migrate implemented method getAllCategories to return a Page<CategoryDto> which receives two parameters.

--- src/main/java/com/alkemy/ong/service/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/CategoryServiceImpl.java
@@ -1,14 +1,21 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.Utils.PageUtils;
+import com.alkemy.ong.dto.CategoryDto;
 import com.alkemy.ong.model.Category;
+import com.alkemy.ong.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class CategoryServiceImpl implements CategoryService{
-
-
+    private final ModelMapper modelMapper;
+    private final CategoryRepository categoryRepository;
     @Override
     public Category getCategory(Long id) {
         return null;
@@ -27,5 +34,12 @@ public class CategoryServiceImpl implements CategoryService{
     @Override
     public Category updateCategory(Category category, Long id) {
         return null;
+    }
+
+    @Override
+    public Page<CategoryDto> getAllCategories(int page, String order) {
+        Page<Category> category = categoryRepository.findAll( PageUtils.getPageable( page, order ) );
+        Page<CategoryDto> categorDtoPage = category.map( cat -> modelMapper.map( cat, CategoryDto.class));
+        return categorDtoPage;
     }
 }
