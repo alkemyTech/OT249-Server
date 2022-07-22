b4ca7ff298cf7d27c714a6511597ad581e961095
Juan Pablo Ferreyra <jpferreyra84@yahoo.com>
2022-07-22 19:29:28 -0300

    OT249-45. New Controller and DTO added.

---
+++ src/main/java/com/alkemy/ong/dto/NewDTO.java
@@ -0,0 +1,23 @@
+package com.alkemy.ong.dto;
+
+import com.alkemy.ong.model.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class NewDTO {
+
+    private String id;
+    private String name;
+    private String content;
+    private String image;
+    private LocalDateTime timestamp;
+    private Category category;
+    private boolean softDelete;
+
+}
