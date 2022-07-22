0fd52ef162fe2dea3ff486101e9c7f228b5fafa7
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-22 19:15:55 -0300

    OT249-40 Migrate to PagingAndSorting to use Paging.

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -2,9 +2,10 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category,Long> {
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
 }
