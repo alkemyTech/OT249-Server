954a16627b55669218585f3071e8552b5a70f27a
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-25 13:30:15 -0300

    OT249-47: Secured End Point based on ADMIN Authority.

--- src/main/java/com/alkemy/ong/controller/NewController.java
+++ src/main/java/com/alkemy/ong/controller/NewController.java
@@ -4,6 +4,7 @@ import com.alkemy.ong.dto.NewDTO;
 import com.alkemy.ong.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +24,7 @@ public class NewController {
 
     }
     @PutMapping("/news/{id}")
+    @PreAuthorize( "hasAuthority('ADMIN')" )
     public ResponseEntity<NewDTO> updateNews(@PathVariable String id, @Valid @RequestBody NewDTO newsDTO, BindingResult bindingResult){
 
         NewDTO newsDTOresponse = newsService.updateNews(id, newsDTO, bindingResult );
