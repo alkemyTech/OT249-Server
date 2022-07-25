93a725af4bb70df41babbe0ca5e9a2f78d88ac6d
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-25 13:17:08 -0300

    OT249-47: Changed Signature method of updateNews on account of needing bindingResult.

--- src/main/java/com/alkemy/ong/service/NewsService.java
+++ src/main/java/com/alkemy/ong/service/NewsService.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.NewDTO;
 import com.alkemy.ong.model.News;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
 
@@ -8,5 +10,6 @@ public interface NewsService {
     News getNews(String id);
     List<News> getAllNews();
     void deleteNews(String id);
-    News updateNews(News news, String id);
+
+    NewDTO updateNews(String id, NewDTO newsDTO, BindingResult bindingResult);
 }
