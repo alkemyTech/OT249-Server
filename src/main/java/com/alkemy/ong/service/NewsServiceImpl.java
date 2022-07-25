835096ec2c3489d4864dd40845b8403cf175f83d
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-25 13:17:40 -0300

    OT249-47: Implemented Canged Signature method and added saving functionality.

--- src/main/java/com/alkemy/ong/service/NewsServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/NewsServiceImpl.java
@@ -1,9 +1,14 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.NewDTO;
+import com.alkemy.ong.exceptions.BindingResultException;
+import com.alkemy.ong.exceptions.RecordException;
 import com.alkemy.ong.model.News;
 import com.alkemy.ong.repository.NewsRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
 
@@ -12,6 +17,7 @@ import java.util.List;
 public class NewsServiceImpl implements NewsService {
 
     private final NewsRepository newsRepository;
+    private final ModelMapper modelMapper;
     @Override
     public News getNews(String id) {
 
@@ -30,8 +36,17 @@ public class NewsServiceImpl implements NewsService {
     }
 
     @Override
-    public News updateNews(News news, String id) {
-
-        return null;
+    public NewDTO updateNews(String id, NewDTO newsDTO, BindingResult bindingResult) {
+        if(bindingResult.hasFieldErrors())
+            throw new BindingResultException( bindingResult );
+        News found = newsRepository.findById( id ).orElseThrow( () -> new RecordException.RecordNotFoundException( "News not found" ) );
+        NewDTO newDTO = this.modelMapper.map(found, NewDTO.class);
+        newDTO.setContent( newsDTO.getContent() );
+        newDTO.setImage( newsDTO.getImage() );
+        newDTO.setName( newsDTO.getName() );
+        News newsToSave = this.modelMapper.map(newDTO, News.class);
+        News savedNews = newsRepository.save( newsToSave );
+        return this.modelMapper.map(savedNews, NewDTO.class);
     }
+
 }
