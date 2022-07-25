d06ac151d17e22e3bf541684c3c2a545cd15317d
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-25 20:09:47 -0300

    OT249-47: Migrate to UUID

--- src/main/java/com/alkemy/ong/dto/NewDTO.java
+++ src/main/java/com/alkemy/ong/dto/NewDTO.java
@@ -6,13 +6,14 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class NewDTO {
 
-    private String id;
+    private UUID id;
     @NotBlank
     private String name;
     @NotBlank
