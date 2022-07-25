a154d9133e5a9f35642a2a1afa963761db548569
Juan Pablo Ferreyra <jpferreyra84@yahoo.com>
2022-07-25 11:57:57 -0300

    OT249-45. Controller and Service completed.

--- src/main/java/com/alkemy/ong/controller/NewController.java
+++ src/main/java/com/alkemy/ong/controller/NewController.java
@@ -3,25 +3,26 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.NewDTO;
 import com.alkemy.ong.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/news")
 public class NewController {
 
     @Autowired
     private NewsService newsService;
 
-    @GetMapping("/news/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<NewDTO> NewDetail(@PathVariable UUID id){
 
         NewDTO newDTO = newsService.getNews(id); //TODO Ver Servicio de News (Id como String)
 
-
+        return ResponseEntity.ok().body(newDTO);
     }
 
 }
