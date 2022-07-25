525321bbb57153f769645bf5080157bbc3dc2e0f
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-25 13:18:46 -0300

    OT249-47: Added end-point for updateNews as /news/{id}.

--- src/main/java/com/alkemy/ong/controller/NewController.java
+++ src/main/java/com/alkemy/ong/controller/NewController.java
@@ -4,10 +4,10 @@ import com.alkemy.ong.dto.NewDTO;
 import com.alkemy.ong.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -21,7 +21,11 @@ public class NewController {
 
         NewDTO newDTO = newsService.getNews(id); //TODO Ver Servicio de News (Id como String)
 
-
     }
+    @PutMapping("/news/{id}")
+    public ResponseEntity<NewDTO> updateNews(@PathVariable String id, @Valid @RequestBody NewDTO newsDTO, BindingResult bindingResult){
 
+        NewDTO newsDTOresponse = newsService.updateNews(id, newsDTO, bindingResult );
+        return ResponseEntity.ok( newsDTOresponse );
+    }
 }
