6e45660025e164a8e9405925c7e6592c71bbc8b8
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-27 14:53:19 -0300

    OT249-Tests Configured

--- src/test/java/com/alkemy/ong/OngApplicationTests.java
+++ src/test/java/com/alkemy/ong/OngApplicationTests.java
@@ -2,8 +2,10 @@ package com.alkemy.ong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = {"test"})
 class OngApplicationTests {
 
 	@Test
