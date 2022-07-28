e8f6f3152a43d9e794a5f70d4e6174f55fb2f252
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-07-28 11:09:01 -0300

    OT249-&5: Added Needed Role

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -5,6 +5,7 @@ import com.alkemy.ong.service.TestimonialService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import javax.validation.Valid;
 public class TestimonialController {
     private final TestimonialService testimonialService;
     @PostMapping("")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<TestimonialDto> createTestimony(@Valid @RequestBody TestimonialDto testimonialDto, BindingResult bindingResult) {
 
         TestimonialDto newTestimonialDto = testimonialService.createTestimony( testimonialDto, bindingResult);
