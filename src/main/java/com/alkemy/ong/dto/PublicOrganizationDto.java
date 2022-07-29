36a3ba3f0e3c6aade2e76df4d90ad2cdc7edd333
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-07-29 15:02:44 -0300

    OT249-37 COlocando nuevamente

--- src/main/java/com/alkemy/ong/dto/PublicOrganizationDto.java
+++ src/main/java/com/alkemy/ong/dto/PublicOrganizationDto.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PublicOrganizationDto {
+        @NotBlank
         private String name;
+        @NotBlank
         private String image;
+        @NotBlank
         private Integer phone;
+        @NotBlank
         private String address;
 
 }
