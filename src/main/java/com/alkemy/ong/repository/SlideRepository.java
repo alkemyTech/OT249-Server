86c121ad590c2fd63623112d9f27b21f71953517
Juan Pablo Ferreyra <jpferreyra84@yahoo.com>
2022-08-02 16:00:44 -0300

    OT249-60. Slides for Ong Added. Role security added in ong.

--- src/main/java/com/alkemy/ong/repository/SlideRepository.java
+++ src/main/java/com/alkemy/ong/repository/SlideRepository.java
@@ -5,9 +5,11 @@ import com.alkemy.ong.model.Slide;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.List;
 
 @Repository
 public interface SlideRepository extends JpaRepository<Slide, String> {
     Slide findTopByOrderByPositionDesc();
+
+    List<Slide> findByOrganization_idLikeOrderByPositionDesc(String ongId);
 }
