c511997b5b15a91a0d26737c191f848b95de73bf
Guido Nicolini <guidonicolini123@hotmail.com>
2022-08-03 07:09:27 -0300

    Agregar documentación con Swagger

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -3,16 +3,12 @@ package com.alkemy.ong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.alkemy.ong.config.SwaggerConfiguration;
 
 @SpringBootApplication()
 public class OngApplication{
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(OngApplication.class, args);
 		System.out.println("Running spring app");
 	}
