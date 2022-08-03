e5ff030dbba4c480118e07f397dd16c43242c518
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-08-03 18:38:14 -0300

    Update OngApplication.java

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication()
 public class OngApplication {
 
-	public static void main(String[] args) {
+	public sttic void main(String[] args) {
 		SpringApplication.run(OngApplication.class, args);
 		System.out.println("Running spring app");
 	}
