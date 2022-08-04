b89f739ffa83a34669e2105398e4b9ef57b1b868
mirema975 <emanuelmiranda975@gmail.com>
2022-08-04 19:03:28 -0300

    OT249-72-create-variables-reduce-memory

--- src/main/java/com/alkemy/ong/Utils/UserSeedData.java
+++ src/main/java/com/alkemy/ong/Utils/UserSeedData.java
@@ -4,30 +4,29 @@ import java.sql.Timestamp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.alkemy.ong.model.Role;
 import com.alkemy.ong.model.User;
 import com.alkemy.ong.repository.RoleRepository;
 import com.alkemy.ong.repository.UserRepository;
-import com.alkemy.ong.service.IRoleService;
 
 @Component
 public class UserSeedData implements CommandLineRunner {
-
+    
 	@Autowired
 	private UserRepository userRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
     private RoleRepository roleRepository;
-    
-    @Autowired
-    private IRoleService roleService;
 	
+    private static final String PASSWORD_USER = "$2a$12$sfmceDRaaF5MKFQeV1LP7.iJuFd.dteTrCw0qw31fM6R54pkqvz5K"; 
+
+    private static final String PASSWORD_ADMIN = "$2a$12$sfmceDRaaF5MKFQeV1LP7.iJuFd.dteTrCw0qw31fM6R54pkqvz5K";
+
+    private static final Timestamp CREATED_AT = new Timestamp(System.currentTimeMillis());
+    private static final Boolean IS_DELETED = false;
+
 	@Override
 	public void run(String... args) throws Exception {
         this.saveRole();
@@ -36,43 +35,47 @@ public class UserSeedData implements CommandLineRunner {
 
     private void saveRole(){
         if(roleRepository.count() == 0){
-            Role roleUser = new Role();
-            roleUser.setName("USER");
-            roleUser.setDescription("Usuario común logueado");
-            roleUser.setTimestamp(new Timestamp(System.currentTimeMillis()));
-            roleRepository.save(roleUser);
+            Role newRoleUser = new Role();
+            newRoleUser.setName("USER");
+            newRoleUser.setDescription("Usuario común logueado");
+            newRoleUser.setTimestamp(CREATED_AT);
+            roleRepository.save(newRoleUser);
 
-            Role roleAdmin = new Role();
-            roleAdmin.setName("ADMIN");
-            roleAdmin.setDescription("Usuario administrador con acceso total");
-            roleAdmin.setTimestamp(new Timestamp(System.currentTimeMillis()));
-            roleRepository.save(roleAdmin);
+            Role newRoleAdmin = new Role();
+            newRoleAdmin.setName("ADMIN");
+            newRoleAdmin.setDescription("Usuario administrador con acceso total");
+            newRoleAdmin.setTimestamp(CREATED_AT);
+            roleRepository.save(newRoleAdmin);
         }
     }
 
 	private void saveUsers() {
 		if (userRepository.count() == 0) {
-			User user1 = new User("Alejandro", "Iglesias", "alejandroIglesias@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/alejandro", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-			User user2 = new User("Yassine", "Bustamante", "yassine@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/yassine", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-			User user3 = new User("Delia", "Serna", "delia@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/delia", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-			User user4 = new User("Adrian", "Moreno", "adrian@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/adrian", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-			User user5 = new User("Jairo", "Riquelme", "jairo@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/jairo", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-            User user6 = new User("Nelson", "Ochoa", "nelson@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/nelson", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-            User user7 = new User("Adolfo", "Barroso", "adolfo@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/adolfo", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-            User user8 = new User("Sara", "Puig", "sara@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/sara", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-            User user9 = new User("Teofilo", "Manrique", "teofilo@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/teofilo", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
-            User user10 = new User("Marisa", "Camara", "marisa@example.com", passwordEncoder.encode("1234"), "http://www.google.com/image/marisa", roleService.getRoleByName("USER"), new Timestamp(System.currentTimeMillis()), false);
 
-            User admin1 = new User("Delia", "Cobo", "deila@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/deila", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-			User admin2 = new User("Hayat", "Bermudez", "hayat@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/hayat", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-			User admin3 = new User("Francisco", "Oviedo", "francisco@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/francisco", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-			User admin4 = new User("Facundo", "Perez", "facundo@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/facundo", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-			User admin5 = new User("Lautaro", "Ortigoza", "lautaro@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/lautaro", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-            User admin6 = new User("Alexis", "Ramirez", "alexis@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/alexis", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-            User admin7 = new User("Ignacio", "Gomez", "ignacio@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/ignacio", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-            User admin8 = new User("Ezequiel", "Ruiz", "ezequiel@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/ezequiel", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-            User admin9 = new User("Agustin", "Bermudez", "agustin@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/agustin", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
-            User admin10 = new User("Esteban", "Lopez", "esteban@example.com", passwordEncoder.encode("admin"), "http://www.google.com/image/esteban", roleService.getRoleByName("ADMIN"), new Timestamp(System.currentTimeMillis()), false);
+            Role roleUser = roleRepository.findByName("USER");
+            Role roleAdmin = roleRepository.findByName("ADMIN");
+
+			User user1 = new User("Alejandro", "Iglesias", "alejandroIglesias@example.com", PASSWORD_USER, "http://www.google.com/image/alejandro", roleUser, CREATED_AT, IS_DELETED);
+			User user2 = new User("Yassine", "Bustamante", "yassine@example.com", PASSWORD_USER, "http://www.google.com/image/yassine", roleUser, CREATED_AT, IS_DELETED);
+			User user3 = new User("Delia", "Serna", "delia@example.com", PASSWORD_USER, "http://www.google.com/image/delia", roleUser, CREATED_AT, IS_DELETED);
+			User user4 = new User("Adrian", "Moreno", "adrian@example.com", PASSWORD_USER, "http://www.google.com/image/adrian", roleUser, CREATED_AT, IS_DELETED);
+			User user5 = new User("Jairo", "Riquelme", "jairo@example.com", PASSWORD_USER, "http://www.google.com/image/jairo", roleUser, CREATED_AT, IS_DELETED);
+            User user6 = new User("Nelson", "Ochoa", "nelson@example.com", PASSWORD_USER, "http://www.google.com/image/nelson", roleUser, CREATED_AT, IS_DELETED);
+            User user7 = new User("Adolfo", "Barroso", "adolfo@example.com", PASSWORD_USER, "http://www.google.com/image/adolfo", roleUser, CREATED_AT, IS_DELETED);
+            User user8 = new User("Sara", "Puig", "sara@example.com", PASSWORD_USER, "http://www.google.com/image/sara", roleUser, CREATED_AT, IS_DELETED);
+            User user9 = new User("Teofilo", "Manrique", "teofilo@example.com", PASSWORD_USER, "http://www.google.com/image/teofilo", roleUser, CREATED_AT, IS_DELETED);
+            User user10 = new User("Marisa", "Camara", "marisa@example.com", PASSWORD_USER, "http://www.google.com/image/marisa", roleUser, CREATED_AT, IS_DELETED);
+
+            User admin1 = new User("Delia", "Cobo", "deila@example.com", PASSWORD_ADMIN, "http://www.google.com/image/deila", roleAdmin, CREATED_AT, IS_DELETED);
+			User admin2 = new User("Hayat", "Bermudez", "hayat@example.com", PASSWORD_ADMIN, "http://www.google.com/image/hayat", roleAdmin, CREATED_AT, IS_DELETED);
+			User admin3 = new User("Francisco", "Oviedo", "francisco@example.com", PASSWORD_ADMIN, "http://www.google.com/image/francisco", roleAdmin, CREATED_AT, IS_DELETED);
+			User admin4 = new User("Facundo", "Perez", "facundo@example.com", PASSWORD_ADMIN, "http://www.google.com/image/facundo", roleAdmin, CREATED_AT, IS_DELETED);
+			User admin5 = new User("Lautaro", "Ortigoza", "lautaro@example.com", PASSWORD_ADMIN, "http://www.google.com/image/lautaro", roleAdmin, CREATED_AT, IS_DELETED);
+            User admin6 = new User("Alexis", "Ramirez", "alexis@example.com", PASSWORD_ADMIN, "http://www.google.com/image/alexis", roleAdmin, CREATED_AT, IS_DELETED);
+            User admin7 = new User("Ignacio", "Gomez", "ignacio@example.com", PASSWORD_ADMIN, "http://www.google.com/image/ignacio", roleAdmin, CREATED_AT, IS_DELETED);
+            User admin8 = new User("Ezequiel", "Ruiz", "ezequiel@example.com", PASSWORD_ADMIN, "http://www.google.com/image/ezequiel", roleAdmin, CREATED_AT, IS_DELETED);
+            User admin9 = new User("Agustin", "Bermudez", "agustin@example.com", PASSWORD_ADMIN, "http://www.google.com/image/agustin", roleAdmin, CREATED_AT, IS_DELETED);
+            User admin10 = new User("Esteban", "Lopez", "esteban@example.com", PASSWORD_ADMIN, "http://www.google.com/image/esteban", roleAdmin, CREATED_AT, IS_DELETED);
 
             userRepository.save(user1);
             userRepository.save(user2);
