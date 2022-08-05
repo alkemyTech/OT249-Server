157e93c68294ea58d0d8d6df509f49274f8a113e
Emanuel991 <emanuelsosa@gmail.com>
2022-08-05 11:28:00 -0300

    OT249-75 list all comments ordered by timestamp

---
+++ src/main/java/com/alkemy/ong/dto/CommentDto.java
@@ -0,0 +1,15 @@
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class CommentDto {
+	//private String id;
+	private String body;
+}
--- src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
@@ -1,19 +1,30 @@
 package com.alkemy.ong.service.impl;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alkemy.ong.dto.CommentDto;
 import com.alkemy.ong.model.Comment;
+import com.alkemy.ong.repository.CommentRepository;
 import com.alkemy.ong.service.CommentService;
 
 @Service
 public class CommentServiceImpl implements CommentService{
+	
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	@Override
-	public Optional<Comment> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<CommentDto> getAll() {
+		List<Comment> commentList = commentRepository.findAllByOrderByTimestampDesc();
+		return commentList.stream().map(comment -> modelMapper.map(comment, CommentDto.class)).collect(Collectors.toList());
 	}
 
 	@Override
