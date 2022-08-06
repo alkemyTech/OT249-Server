f677251d938bdfe24dec605763e4054467a7617c
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-08-06 17:38:48 -0300

    Docs for testimonials methods

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -2,6 +2,11 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.TestimonialDto;
 import com.alkemy.ong.service.TestimonialService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +20,38 @@ import javax.validation.Valid;
 @RequestMapping(value = "/testimonials")
 @AllArgsConstructor
 public class TestimonialController {
-    private final TestimonialService testimonialService;
-
     @PostMapping("")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Endpoint to create Testimony")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Testimonial created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TestimonialDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Testimonial not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Acceso no autorizado",
+            content = @Content) })
     public ResponseEntity<TestimonialDto> createTestimony(@Valid @RequestBody TestimonialDto testimonialDto, BindingResult bindingResult) {
 
         TestimonialDto newTestimonialDto = testimonialService.createTestimony( testimonialDto, bindingResult);
         return new ResponseEntity<>( newTestimonialDto, HttpStatus.CREATED);
     }
+
+    private final TestimonialService testimonialService;
+
+    @Operation(summary = "Endpoint to update Testimony")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Testimonial uddated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TestimonialDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Testimonial not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Acceso no autorizado",
+                    content = @Content) })
     @PutMapping("/{id}")
     @PreAuthorize( "hasRole('ADMIN')" )
     public ResponseEntity<TestimonialDto> updateTestimonial(@PathVariable String id, @Valid @RequestBody TestimonialDto testimonialDto, BindingResult bindingResult){
@@ -31,6 +59,18 @@ public class TestimonialController {
         TestimonialDto updateTestimony = testimonialService.updateTestimony(id, testimonialDto, bindingResult );
         return ResponseEntity.ok( updateTestimony );
     }
+
+    @Operation(summary = "Endpoint to delete Testimony")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Testimonial deleted",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TestimonialDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Testimonial not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Acceso no autorizado",
+                    content = @Content) })
     @DeleteMapping("/{id}")
     @PreAuthorize( "hasRole('ADMIN')" )
     public ResponseEntity<?> deleteTestimonial(@PathVariable String id){
