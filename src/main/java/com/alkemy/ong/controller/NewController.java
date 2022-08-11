04d5b952f575143538b96ff986fa4e2d7e940767
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-08-11 19:44:56 -0300

    OT249-93: Test: Endpoints /news

--- src/main/java/com/alkemy/ong/controller/NewController.java
+++ src/main/java/com/alkemy/ong/controller/NewController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/news")
+@AllArgsConstructor
 public class NewController {
 
 	@Autowired
